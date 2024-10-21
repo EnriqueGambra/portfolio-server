3bd926920c9b2f16fb80d68d07d49667fcc1ce26
enriquegambra <gambra7@gmail.com>
2024-10-21 13:58:10 -0400

    initial commit.

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import { Octokit } from "@octokit/rest";
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const octokit = new Octokit({
+    auth: process.env.API_KEY,
+});
+
+const app = express();
+
+
+app.listen(process.env.PORT, () => {
+
+});
+
+app.use(cors());
+
+app.get("/getRepoData", async (req, res, next) => {
+
+    let response = { }
+
+    const cpuSimulatorData = await octokit.request("GET /repos/enriquegambra/CPU_Simulator");
+
+    response['cpuSimulatorData'] = {
+        name: cpuSimulatorData.data.name,
+        description: cpuSimulatorData.data.description,
+        html_url: cpuSimulatorData.data.html_url
+    };
+
+    const amazonAlexaData = await octokit.request("GET /repos/enriquegambra/Amazon-Alexa-Skill");
+
+    response['amazonAlexaData'] = {
+        name: amazonAlexaData.data.name,
+        description: amazonAlexaData.data.description,
+        html_url: amazonAlexaData.data.html_url,
+    };
+
+    const nbaPlayerStats = await octokit.request("GET /repos/enriquegambra/Compare-NBA-Players-Stats");
+
+    response['nbaPlayerStats'] = {
+        name: nbaPlayerStats.data.name,
+        description: nbaPlayerStats.data.description,
+        html_url: nbaPlayerStats.data.html_url,
+    };
+
+    const alienInvasionData = await octokit.request("GET /repos/enriquegambra/Alien-Invasion");
+
+    response['alienInvasionData'] = {
+        name: alienInvasionData.data.name,
+        description: alienInvasionData.data.description,
+        html_url: alienInvasionData.data.html_url,
+    }
+
+    res.json(response);
+
+    return res;
+});
